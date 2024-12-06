bd0b1263e2bf7fb1b4362c560be9f47128d37b20
Youness <youness.hassani@4d.com>
2024-12-06 11:49:22 +0100

    chore: project setup using Vite

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="h-32 p-2 pl-4 text-green-300">Hello world!</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
